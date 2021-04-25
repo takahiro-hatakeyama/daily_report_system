31f9a696d92ff1185bbba0b7189d90920fe304fe
takahiro.hatakeyama <hatakeyama.takahiro.0704@gmail.com>
2021-04-25 16:52:56 +0900

    Add Employee show

--- src/controllers/employees/EmployeesCreateServlet.java
+++ src/controllers/employees/EmployeesCreateServlet.java
@@ -44,8 +44,7 @@ public class EmployeesCreateServlet extends HttpServlet {
             e.setCode(request.getParameter("code"));
             e.setName(request.getParameter("name"));
             e.setPassword(
-                EncryptUtil.getPasswordEncrypt(
-                    request.getParameter("password"),
+                EncryptUtil.getPasswordEncrypt(request.getParameter("password"),
                         (String)this.getServletContext().getAttribute("pepper")
                     )
                 );
